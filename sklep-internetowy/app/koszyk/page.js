1495460b8a6fe8d308fbbfed54e45becd8479b73
Marta Piotrowska <m.piotrowska.257@studms.ug.edu.pl>
2025-02-14 00:43:09 +0100

    Refactor Home components for improved data fetching and error handling; update filter functionality and enhance product display logic

--- sklep-internetowy/app/koszyk/page.js
+++ sklep-internetowy/app/koszyk/page.js
@@ -1,35 +1,35 @@
-// "use client";
+"use client";
 
-// import { use, useEffect, useState } from "react";
-// import Link from "next/link";
-// // import styles from "./koszyk.module.css";
-// import headers from "@/app/headers";
+import { use, useEffect, useState } from "react";
+import Link from "next/link";
+// import styles from "./koszyk.module.css";
+import headers from "@/app/headers";
 
-// export default function KoszykPage() {
-//   const [products, setProducts] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true);
+export default function KoszykPage() {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-//   useEffect(() => {
-//     fetch("https://api.escuelajs.co/api/v1/products/", {
-//       method: "GET",
-//       headers,
-//     })
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         console.log("Fetched products:", data);
-//         setProducts(data);
-//         setIsLoading(false);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching products:", error);
-//         setIsLoading(false);
-//       });
-//   }, []);
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products/", {
+      method: "GET",
+      headers,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Fetched products:", data);
+        setProducts(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+        setIsLoading(false);
+      });
+  }, []);
 
-//   return <div>koszyk pusty</div>;
-// }
+  return <div>koszyk pusty</div>;
+}
--- sklep-internetowy/app/page.js
+++ sklep-internetowy/app/page.js
@@ -1,66 +1,68 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
 import Link from "next/link";
-import styles from "./home.module.css";
-import headers from "@/app/headers";
-import Filter from "@/app/components/Filter";
+// import "@/app/globals.css";
+import styles from "@/app/home.module.css";
+import Filter from "./components/Filter";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
   useEffect(() => {
-    fetch(`https://api.escuelajs.co/api/v1/products`, {
-      method: "GET",
-      headers,
-    })
-      .then((response) => {
+    async function fetchList() {
+      try {
+        const limit = searchParams.get("limit") || "150";
+        const search = searchParams.get("search") || "";
+        const category = searchParams.get("category") || "";
+
+        const response = await fetch(
+          "https://api.escuelajs.co/api/v1/products"
+        );
         if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+          throw new Error("Could not fetch list");
         }
-        return response.json();
-      })
-      .then((data) => {
-        // console.log("Fetched products:", data);
-        setProducts(data);
+        const data = await response.json();
+        let detailedData = data;
+
+        if (search) {
+          detailedData = detailedData.filter((prod) =>
+            prod.title.toLowerCase().includes(search.toLowerCase())
+          );
+        }
+
+        if (category) {
+          detailedData = detailedData.filter(
+            (prod) =>
+              prod.category.name.toLowerCase() === category.toLowerCase()
+          );
+        }
+
+        detailedData = detailedData.slice(0, limit);
+        setProducts(detailedData);
+      } catch (error) {
+        setErrorMessage(error);
+      } finally {
         setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-        setIsLoading(false);
-      });
-  }, []);
-
-  // products.map((product) => {
-  //   const images = product.images;
-  //   const cleanImages = JSON.parse(images[0].replace(/\\\"/g, '"'));
-  //   console.log(product.images[0]);
-  // });
-
-  const handleFilters = ({ search, price, category, limit }) => {
-    let filtered = [...products];
-
-    if (search) {
-      filtered = filtered.filter((product) =>
-        product.title.toLowerCase().includes(search.toLowerCase())
-      );
+      }
     }
 
-    if (price) {
-      filtered = filtered.filter((product) => product.price === price);
-    }
+    fetchList();
+  }, [searchParams]);
 
-    if (category) {
-      filtered = filtered.filter(
-        (product) => product.category.name === category
-      );
-    }
+  const updateFilters = (filters) => {
+    const params = new URLSearchParams();
 
-    setFilteredProducts(
-      filtered.slice(0, Math.max(1, parseInt(limit, 10) || 20))
-    );
+    if (filters.search) params.set("search", filters.search);
+    if (filters.category) params.set("category", filters.category);
+    if (filters.limit) params.set("limit", filters.limit);
+
+    router.push(`/?${params.toString()}`);
   };
 
   return (
@@ -70,14 +72,14 @@ export default function Home() {
         <p>Ładowanie...</p>
       ) : (
         <>
-          <Filter updateFilters={handleFilters} />
+          <Filter updateFilters={updateFilters} />
           <div className={styles.productGrid}>
             {products.map((product) => (
               <div key={product.id} className={styles.productCard}>
                 <Link href={`/${product.id}`}>
                   <img
                     src={product.images.map((image) =>
-                      image.replace(/\\\"/g, '"')
+                      JSON.parse(image.replace(/\\\"/g, '"'))
                     )}
                     alt={product.title}
                     className={styles.productImage}
